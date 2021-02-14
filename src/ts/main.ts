51cf1b8c127dc7ac59ee605c3f01547c7463d88b
Z3r0cks <patrick.kaserer@web.de>
2021-02-14 12:04:17 +0100

    FIX: chome storage sync

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,48 +3,49 @@
 /// <reference path="./Storage.ts" />
 namespace BookmarkExtension {
 
-   Storage.replaceBodyInnerHtml();
+   //TODO: Kein Hinzufügen sobald keine URL eingegeben wurde
 
-   const getSyncStorage: HTMLButtonElement = document.createElement("button");
-   const deleteSycnStorage: HTMLButtonElement = document.createElement("button");
-   const saveBodyInnerHtml: HTMLButtonElement = document.createElement("button");
-   const replaceBodyInnerHtml: HTMLButtonElement = document.createElement("button");
+   // Storage.replaceBodyInnerHtml();
+   function createDevButtons() {
+      const getSyncStorage: HTMLButtonElement = document.createElement("button");
+      const deleteSycnStorage: HTMLButtonElement = document.createElement("button");
+      const saveBodyInnerHtml: HTMLButtonElement = document.createElement("button");
+      const replaceBodyInnerHtml: HTMLButtonElement = document.createElement("button");
 
+      deleteSycnStorage.innerHTML = "Delete Sync Dev Storage";
+      getSyncStorage.innerHTML = "Get Sync Dev Storage";
+      saveBodyInnerHtml.innerHTML = "Save Body InnerHTML";
+      replaceBodyInnerHtml.innerHTML = "Replace Body InnerHtml";
 
-   deleteSycnStorage.innerHTML = "Delete Sync Dev Storage";
-   getSyncStorage.innerHTML = "Get Sync Dev Storage";
-   saveBodyInnerHtml.innerHTML = "Save Body InnerHTML";
-   replaceBodyInnerHtml.innerHTML = "Replace Body InnerHtml";
+      replaceBodyInnerHtml.addEventListener("click", () => {
+         Storage.replaceBodyInnerHtml();
+      })
 
-   replaceBodyInnerHtml.addEventListener("click", () => {
-      Storage.replaceBodyInnerHtml();
-   })
+      saveBodyInnerHtml.addEventListener("click", () => {
+         Storage.saveBodyInnerHtml();
+      })
 
-   saveBodyInnerHtml.addEventListener("click", () => {
-      Storage.saveBodyInnerHtml();
-   })
+      deleteSycnStorage.addEventListener("click", () => {
+         Storage.deleteSycnStorage();
+      })
 
-   deleteSycnStorage.addEventListener("click", () => {
-      Storage.deleteSycnStorage();
-   })
+      getSyncStorage.addEventListener("click", () => {
+         Storage.getSyncStorage();
+      })
 
-   getSyncStorage.addEventListener("click", () => {
-      Storage.getSyncStorage();
-   })
-
+      document.body.appendChild(deleteSycnStorage);
+      document.body.appendChild(getSyncStorage);
+      document.body.appendChild(saveBodyInnerHtml);
+      document.body.appendChild(replaceBodyInnerHtml);
+   }
+   createDevButtons();
 
    const newAddBtn = document.createElement("button");
    newAddBtn.className = "addNewCategory"
    newAddBtn.innerHTML = "Add Category"
 
    const wrapper: HTMLElement = document.getElementById("wrapper");
-   document.body.appendChild(deleteSycnStorage);
-   document.body.appendChild(getSyncStorage);
-   document.body.appendChild(saveBodyInnerHtml);
-   document.body.appendChild(replaceBodyInnerHtml);
-
    document.body.appendChild(newAddBtn);
-
    newAddBtn.addEventListener('click', () => {
       const newCategory: Category = new Category();
       newCategory.addNewCategory();
